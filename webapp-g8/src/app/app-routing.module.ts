a8b9cd239402a81f791c6c68187095adf3abb9e5
carlos david <2111724640101@ingenieria.usac.edu.gt>
2021-03-26 23:16:55 -0600

    Se implemento la recuepracion de contrasenia

--- webapp-g8/src/app/app-routing.module.ts
+++ webapp-g8/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecuperarContraseniaComponent } from './Component/recuperar-contrasenia/recuperar-contrasenia.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: '/login',
     pathMatch: 'full'
+  },
+  {
+    path: 'recuperarContrasenia',
+    component : RecuperarContraseniaComponent
   }
 ];
 @NgModule({
--- webapp-g8/src/app/app.module.ts
+++ webapp-g8/src/app/app.module.ts
@@ -5,10 +5,12 @@ import {FormsModule}from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RecuperarContraseniaComponent } from './Component/recuperar-contrasenia/recuperar-contrasenia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecuperarContraseniaComponent
   ],
   imports: [
     BrowserModule,
