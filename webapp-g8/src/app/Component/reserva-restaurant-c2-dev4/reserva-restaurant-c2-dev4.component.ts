834f25008b621d2495ebc75b95967f64ff65bc6b
Romario Castillo <romariocastillo1994@gmail.com>
2021-03-27 00:04:16 -0600

    Se creó el componente reserva de restaurante.

---
+++ webapp-g8/src/app/Component/reserva-restaurant-c2-dev4/reserva-restaurant-c2-dev4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reserva-restaurant-c2-dev4',
+  templateUrl: './reserva-restaurant-c2-dev4.component.html',
+  styleUrls: ['./reserva-restaurant-c2-dev4.component.css']
+})
+export class ReservaRestaurantC2Dev4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
