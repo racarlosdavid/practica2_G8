9106e41b2ac0affbe9fc461dea0856cf136f0094
Sebastian <marcoscabrera96@gmail.com>
2021-03-27 21:47:29 -0600

    Se agregó la funcionalidad de calificar Hotel

--- webapp-g8/src/app/Component/calificar-hotel/calificar-hotel.component.ts
+++ webapp-g8/src/app/Component/calificar-hotel/calificar-hotel.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+function verificarCampos(valor1,valor2): boolean {
+  if (
+    (valor1==null)||(valor2==null)||(valor2=='')
+  ){
+    return false;
+  }else{
+    return true;
+  }
+}
+
+function crearCalificacion(x1:number,x2:string):Calificacion{
+  return new Calificacion(x1,x2);
+}
+
 @Component({
   selector: 'app-calificar-hotel',
   templateUrl: './calificar-hotel.component.html',
   styleUrls: ['./calificar-hotel.component.css']
 })
-export class CalificarHotelComponent implements OnInit {
 
-  constructor() { }
+export class CalificarHotelComponent {
+  puntaje=null;
+  opinion=null;
 
-  ngOnInit(): void {
+  calificacion=null;
+
+  calificar(){
+    var verificar:boolean=verificarCampos(this.puntaje,this.opinion);
+    if (verificar){
+      alert('Calificacion: '+this.puntaje+' estrellas\n\n'
+        +this.opinion);
+      
+        this.calificacion=crearCalificacion(this.puntaje,this.opinion);
+        
+    }else{
+      alert('Debe ingresar una calificacion entre 1 y 5 entrellas, y debe dejar una opinion sobre el hotel');
+    }
   }
-
 }
+export class Calificacion {
+  constructor(public punteo: number, public comentario: string) {
+  }
+}
+
