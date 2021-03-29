21746b8fadac73f085b6644be9b87495318e395e
Pablo César <pablopachecof.18@gmail.com>
2021-03-29 16:57:40 -0600

    Se realizaron pruebas unitarias con Mocks para el Administrador

--- webapp-g8/src/app/Component/administrador/administrador.component.spec.ts
+++ webapp-g8/src/app/Component/administrador/administrador.component.spec.ts
@@ -95,3 +95,90 @@ describe('AdministradorComponent', () => {
     expect(component.res_costo).toContain('900');*/
   });
 });
+
+//Pruebas con Mocks
+describe('AdministradorComponent - Mock', () => {
+  //Creación de la clase Mock
+  class MockDatos extends AdministradorComponent {
+    //Se reescriben los métodos a utilizar con los valores de retorno esperados
+    Verifcamposllenos(nombre:string, correo:string, contra:string, contra2:string)
+    {
+      return true;
+    }
+    Verifcontra(contra:string, contra2:string)
+    {
+      return 1;
+    }
+    Verifnuser(nom:string)
+    {
+      return 2;
+    }
+  }
+  //Instanciamos variables de control
+  //ComponentFixture es un manejador para probar el entorno de nuestro componente
+  let component: AdministradorComponent;
+  let fixture: ComponentFixture<AdministradorComponent>;
+  //Servicio de router
+  let router: Router;
+  //Nuestro Mock
+  let mock: MockDatos;
+
+  //En los imports se agrega el RouterTestingModule con sus rutas
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: Router,
+          useClass: MockDatos
+        }
+      ],
+      imports: [
+        RouterTestingModule.withRoutes([]),
+        FormsModule
+      ],
+      declarations: [ AdministradorComponent ]
+    })
+    .compileComponents();
+  });
+
+  //Inicializamos las variables de control
+  beforeEach(() => {
+    //Indicamos a fixture el componente a manejar testeo
+    fixture = TestBed.createComponent(AdministradorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    //TestBed otorga el comportamiento del router
+    router = TestBed.get(Router);
+    //Inicializamos el mock
+    mock = new MockDatos(null);
+  });
+  //-------------Prueba para Verificarbusqueda() .toContain()
+  it('Caso de prueba para: VerReserva(), se espera que el arreglo contena el dato indicado', () => 
+  {
+    const fix = TestBed.createComponent(AdministradorComponent);
+    fix.detectChanges();
+    fix.whenStable().then(() =>
+    {
+      let input = fix.debugElement.query(By.css('#vercliente'));
+      input.nativeElement.value = 'Amelia';
+      let el = input.nativeElement;
+      //Se prueba el componente con tipo de usuario inválido
+      component.VerReserva('a');
+      //Se espera respuesta de alerta si fue llamado
+      expect(component.res_cliente).toContain('Amelia');
+    });   
+  });
+  //-------------Prueba para Verifadmi() .toEqual()
+  it('Caso de prueba con Mock para Verifadmi(), se espera que se agreguen 5 datos al arreglo para un total de 8 almacenados', () => 
+  {
+    //Se llama el método mockeado y se le agregan 5 datos extras (total = 8)
+    mock.addData('Nombreprueba', 'mail1', '1234', '1234');
+    mock.addData('Nombreprueba2', 'mail2', '1234', '1234');
+    mock.addData('Nombreprueba3', 'mail3', '1234', '1234');
+    mock.addData('Nombreprueba4', 'mail4', '1234', '1234');
+    mock.addData('Nombreprueba5', 'mail5', '1234', '1234');
+
+    //Se espera respuesta de alerta si fue llamado
+    expect(mock.list_nom.length).toEqual(8);
+  });
+});
