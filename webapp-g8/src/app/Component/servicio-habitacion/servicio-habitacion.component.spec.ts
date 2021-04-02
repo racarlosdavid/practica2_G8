8856bd5f1a8331cc9984d9b6f22dde70e694810f
Sebastian <marcoscabrera96@gmail.com>
2021-04-02 15:14:32 -0600

    Se realizaron pruebas unitarias para el componente servicioHabitacion

--- webapp-g8/src/app/Component/servicio-habitacion/servicio-habitacion.component.spec.ts
+++ webapp-g8/src/app/Component/servicio-habitacion/servicio-habitacion.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { ServicioHabitacionComponent } from './servicio-habitacion.component';
 
 describe('ServicioHabitacionComponent', () => {
-  let component: ServicioHabitacionComponent;
-  let fixture: ComponentFixture<ServicioHabitacionComponent>;
+  let servicio: ServicioHabitacionComponent;
+  
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ServicioHabitacionComponent ]
-    })
-    .compileComponents();
+    servicio= new ServicioHabitacionComponent();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ServicioHabitacionComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('Prueba para verificarCampos(): se espera false', async (()=>{
+    var x1=null;
+    var x2=0;
+    expect (servicio.verificarCampos(x1,x2)).toEqual(false);
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Prueba para totalPedido(): se espera 44', async (()=>{
+    var x1=22;
+    var x2=2;
+    expect (servicio.totalPedido(x1,x2)).toEqual(44);
+  }));
+
 });
