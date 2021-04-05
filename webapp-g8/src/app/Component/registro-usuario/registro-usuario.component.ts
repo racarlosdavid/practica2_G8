9c9da1e6d9d502d9689b1aee3b1c078886335bd1
carlos david <2111724640101@ingenieria.usac.edu.gt>
2021-04-05 00:32:03 -0600

    Se realizaron pruebas unitarias con mocks para el componente RegistrarUsuario

--- webapp-g8/src/app/Component/registro-usuario/registro-usuario.component.ts
+++ webapp-g8/src/app/Component/registro-usuario/registro-usuario.component.ts
@@ -9,23 +9,69 @@ import { user } from '../../models/user';
 export class RegistroUsuarioComponent implements OnInit {
 
   usuario: user = {        
-    cui : null,              
+    cui : undefined,              
     nombre : '',          
     apellido : '',         
     fecha_nacimiento : null,
     correo : '',
     contrasenia : ''
   };
+  elUsuarioExiste = false;
+  elCUIesNumero = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  setUser(Usu:user){
+    this.usuario = Usu;
+  }
 
   registrarUsuario(){
-    
-    console.log(this.usuario.id, this.usuario)
-
+    console.log("El usuario ", this.usuario.nombre," se ha registrado con exito")
   }
+
+  validarCorreo():number{
+    console.log("Validar el correo -> ",this.usuario.correo)
+    var res = this.usuario.correo.match('^[^@]+@[^@]+\.[a-zA-Z]{2,}$');
+    console.log(res+" "+res.length);
+    
+    if ( res != null && res.length > 0 ) 
+      return res.length;
+    return 0;
+  }
+
+  validadCampoVacio(email:string):boolean{
+    if(email != undefined){
+      return false;
+    }else {
+      return true;
+    }
+  }
+
+  comprobarNingunCampoVacio():boolean{
+    var bandera = true;
+    if(this.usuario.cui == undefined || this.usuario.nombre  == undefined || this.usuario.apellido  == undefined 
+      || this.usuario.fecha_nacimiento  == undefined || this.usuario.correo  == undefined || this.usuario.correo  == undefined ){
+      bandera = false;
+    }
+    return bandera;
+  }
+
+  comprobarUsuarioYaRegistrado(){
+    return this.elUsuarioExiste;
+  }
+
+  comprobarCUIesNumero(){
+    console.log("Validar el cui -> ",this.usuario.cui)
+    if (isNaN(this.usuario.cui)) {
+      this.elCUIesNumero = false;
+    }else{
+      this.elCUIesNumero = true;
+    }
+    return this.elCUIesNumero;
+  }
+ 
 
 }
