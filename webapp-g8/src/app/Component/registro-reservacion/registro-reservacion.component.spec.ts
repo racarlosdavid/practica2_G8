f8f03239df406517107747572f35ef0fffde2ac9
cristian estuardo herrera poncio <2070142720101@ingenieria.usac.edu.gt>
2021-04-04 19:14:18 -0600

    Se realizaron pruebas unitarias para el componente RegistroReservacion

--- webapp-g8/src/app/Component/registro-reservacion/registro-reservacion.component.spec.ts
+++ webapp-g8/src/app/Component/registro-reservacion/registro-reservacion.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistroReservacionComponent } from './registro-reservacion.component';
+import { Reserva } from '../../models/Reserva.model'
 
 describe('RegistroReservacionComponent', () => {
   let component: RegistroReservacionComponent;
@@ -21,5 +22,55 @@ describe('RegistroReservacionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+});
+describe('Prueba verificar campos Reservacion',()=>{
+  it('deberia devolver true',() =>{
+    //ARRANGE
+    const reser = new Reserva("doble","2021","2021",3,12);
+    //ACT
+    const rta = reser.veficarcampos();
+    //ASSERT
+    expect(rta).toBeTrue();
+  });
+});
+describe('Prueba veficiar Fechas Reservacion',()=>{
+  it('deberia devolver true',()=>{
+    //ARRANGE
+    const reser = new Reserva("doble","2021-03-12","2021-03-19",0,0);
+    //ACT
+    const rta = reser.verificarfechas();
+    //ASSERT
+    expect(rta).toBeTrue();
+  });
+});
+describe('Prueba veficiar Fechas Reservacion false',()=>{
+  it('deberia devolver false',()=>{
+    //ARRANGE
+    const reser = new Reserva("doble","2021-03-19","2021-03-12",0,0);
+    //ACT
+    const rta = reser.verificarfechas();
+    //ASSERT
+    expect(rta).toBeFalse();
+  });
+});
+describe('Prueba verificar campos Reservacion False',()=>{
+  it('deberia devolver false',() =>{
+    //ARRANGE
+    const reser = new Reserva("","2021","2021",3,12);
+    //ACT
+    const rta = reser.veficarcampos();
+    //ASSERT
+    expect(rta).toBeFalse();
+  });
+});
+describe('Prueba verificar cantidad de personas',()=>{
+  it('deberia devolver la cantidad 3',() =>{
+    //ARRANGE
+    const reser = new Reserva("","2021","2021",3,12);
+    //ACT
+    const rta = reser.verificarCantidadPersonas();
+    //ASSERT
+    expect(rta).toEqual(3);
   });
 });
